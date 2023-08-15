c62a1c1bff1c404b93ae3e2d1a0f0409d1e29482
Adiya <garudaditya7@gmail.com>
2023-08-15 20:07:49 +0530

    test

--- src/graph/bfs/Main.java
+++ src/graph/bfs/Main.java
@@ -3,44 +3,14 @@ package graph.bfs;
 import com.sun.source.tree.Tree;
 
 import java.util.*;
-
+//https://leetcode.com/problems/find-duplicate-subtrees/
 public class Main {
     public static void main(String[] args) {
         System.out.println("hhii");
     }
 
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        List<TreeNode> res = new LinkedList<>();
-        traverse(root, new HashMap<>(), res);
-        return res;
-    }
 
-    public String traverse(TreeNode node, Map<String, Integer> cnt,
-                           List<TreeNode> res) {
-        if (node == null) {
-            return "";
-        }
-        String representation = "(" + traverse(node.left, cnt, res) + ")" +
-                node.val + "(" + traverse(node.right, cnt, res) +
-                ")";
-        cnt.put(representation, cnt.getOrDefault(representation, 0) + 1);
-        if (cnt.get(representation) == 2) {
-            res.add(node);
-        }
-        return representation;
-    }
 }
 
-class Helper{
-    TreeNode parent;
-    int minRange;
-    int maxRange;
-
-    Helper(TreeNode parent, int minRange, int maxRange){
-        this.parent = parent;
-        this.minRange = minRange;
-        this.maxRange = maxRange;
-    }
-}
 
 
---
+++ src/stack/FindDuplicateSubtrees.java
@@ -0,0 +1,51 @@
+package stack;
+
+import graph.bfs.TreeNode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class FindDuplicateSubtrees {
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        List<TreeNode> res = new LinkedList<>();
+        traverse(root, new HashMap<>(), res);
+        return res;
+    }
+
+    public String traverse(TreeNode node, Map<String, Integer> cnt,
+                           List<TreeNode> res) {
+        if (node == null) {
+            return "";
+        }
+        String representation = "(" + traverse(node.left, cnt, res) + ")" +
+                node.val + "(" + traverse(node.right, cnt, res) +
+                ")";
+        cnt.put(representation, cnt.getOrDefault(representation, 0) + 1);
+        if (cnt.get(representation) == 2) {
+            res.add(node);
+        }
+        return representation;
+    }
+
+    static class TreeNode{
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+}
