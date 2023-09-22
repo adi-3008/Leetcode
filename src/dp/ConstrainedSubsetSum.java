21999ceb981ad9d00952552420edf7c8d475419b
Adiya <garudaditya7@gmail.com>
2023-09-22 11:32:36 +0530

    msg

--- src/dp/ConstrainedSubsetSum.java
+++ src/dp/ConstrainedSubsetSum.java
@@ -1,6 +1,6 @@
 package dp;
 
-import java.util.Arrays;
+import java.util.*;
 
 //https://leetcode.com/problems/constrained-subsequence-sum/description/
 
@@ -9,9 +9,53 @@ public class ConstrainedSubsetSum {
     // O(N^2) TLE
 
     public static void main(String[] args) {
-        int[] nums = {-1, -2, -3};
+        int[] nums = {-1,-2,-3};
         int k = 1;
-        System.out.println(constrainedSubsetSum(nums, k));
+        System.out.println(constrainedSubsetSumHeap(nums, k));
+    }
+
+    public static int constrainedSubsetSumHeap(int[] nums, int k) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            return b[0] - a[0];
+        });
+
+        heap.add(new int[] {nums[0], 0});
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            while (i - heap.peek()[1] > k) {
+                heap.remove();
+            }
+
+            int curr = Math.max(0, heap.peek()[0]) + nums[i];
+            ans = Math.max(ans, curr);
+            heap.add(new int[] {curr, i});
+        }
+
+        return ans;
+    }
+
+    public static int constrainedSubsetSumDeque(int[] nums, int k) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Deque<Integer> deque = new LinkedList<>();
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp[i] = nums[i];
+            if (!deque.isEmpty()) {
+                dp[i] = Math.max(dp[i], nums[i] + dp[deque.peekFirst()]);
+            }
+            while (!deque.isEmpty() && dp[i] > dp[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (i - deque.peekFirst() >= k) {
+                deque.pollFirst();
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
     }
 
     public static int constrainedSubsetSum(int[] nums, int k) {
--- src/heap/MaxScoreOfSubsequence.java
+++ src/heap/MaxScoreOfSubsequence.java
@@ -1,7 +1,45 @@
 package heap;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class MaxScoreOfSubsequence {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 0, 3, 2, 4};
+        int[] nums2 = {2, 1, 3, 4, 5};
+        System.out.println(maxScore(nums1, nums2, 3));
+    }
+
+    public static long maxScore(int[] nums1, int[] nums2, int k) {
+        long maxScore = 0;
+        int n = nums1.length;
+
+        int[][] nums = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = new int[]{nums2[i], nums1[i]};
+        }
+
+        Arrays.sort(nums, (a, b) -> Integer.compare(b[0], a[0]));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        long currSum = 0;
+        for (int i = 0; i < n; i++) {
+            currSum += nums[i][1];
+            pq.add(nums[i][1]);
+
+            if (pq.size() > k){
+                int top = pq.remove();
+                currSum -= top;
+            }
+
+            if (pq.size() == k)
+                maxScore = Math.max(maxScore, currSum * nums[i][0]);
+        }
+        
+        return maxScore;
+    }
 
 }
